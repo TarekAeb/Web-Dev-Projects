8282b4be6ea7cf615ebf37e665509caef8db48d2
TarekAeb <t6tarek@gmail.com>
2024-06-28 10:58:09 +0100

    new

---
+++ Iphone calculator/index.js
@@ -0,0 +1,42 @@
+setInterval(()=>{const date = document.getElementById('clock');
+date.textContent = new Date().getHours() + ":" + new Date().getMinutes();},1000); 
+
+let ans = 0;
+
+const display = document.querySelector('.calculator-display')
+
+function addToDisplay(input){
+    if( ans == 1 && !(input === "+" || input === "-" || input === "/" || input === "*")){
+        clear();
+    }
+    ans = 0;
+    display.value += input;
+    if(display.value === '0'){
+        display.value = input;
+        return;
+    }
+}
+
+function clearDisplay(){
+    display.value = "0";
+}
+
+function calculate(){
+    try{display.value = eval(display.value);
+        ans = 1;
+    }
+    catch(error){
+        display.value = "error";    
+    }    
+}
+
+
+function toogleminus(){
+    if(display.value[0] === '-'){
+        display.value
+    }
+    else {
+        display.value = "-" + display.value;
+
+    }
+}
